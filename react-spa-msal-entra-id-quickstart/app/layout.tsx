fc08a809c4e5830962094159b0121e263ecbf005
Chris Dymond <dwarfered@users.noreply.github.com>
2025-05-12 17:54:21 +0800

    refactor

--- react-spa-msal-entra-id-quickstart/app/layout.tsx
+++ react-spa-msal-entra-id-quickstart/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import ClientFluentProvider from "@/components/ClientFluentProvider";
+import ClientFluentProvider from "@/components/providers/ClientFluentProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
