da86543d3a53d176fe11e241f3811db4c73ef35b
Chris Dymond <dwarfered@users.noreply.github.com>
2025-05-18 11:13:33 +0800

    removal of duplicate msgraph access token function

--- react-spa-msal-entra-id-quickstart/lib/msal/helper.ts
+++ react-spa-msal-entra-id-quickstart/lib/msal/helper.ts
@@ -2,31 +2,6 @@ import { AccountInfo, AuthenticationResult, InteractionRequiredAuthError } from 
 import { loginRequest } from "./config";
 import { msalInstance } from "./instance";
 
-export async function acquireGraphAccessToken(): Promise<string> {
-  const account = msalInstance.getActiveAccount();
-  if (!account) {
-    console.warn("No active account found in acquireGraphAccessToken()");
-    throw new Error("No active account! User may not be signed in.");
-  }
-
-  try {
-    const response = await msalInstance.acquireTokenSilent({
-      ...loginRequest,
-      account,
-    });
-    return response.accessToken;
-  } catch (error: unknown) {
-    if (error instanceof InteractionRequiredAuthError) {
-      console.warn("Token interaction required. Redirecting to sign in.");
-      // msalInstance.clearCache(); // optional — be cautious with this
-      handleSignIn();
-    } else {
-      console.error("Silent token acquisition failed", error);
-    }
-    throw error;
-  }
-}
-
 export function handleSignIn() {
   msalInstance.loginRedirect(loginRequest).catch((e: unknown) => {
     console.error(`loginRedirect failed: ${e}`);
